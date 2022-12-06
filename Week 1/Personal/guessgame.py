a31f5e699c090a7aea7c99fd155c2f92d5ee0430
prasiddhp10 <107663558+prasiddhp10@users.noreply.github.com>
2022-12-06 19:26:52 +0545

    Created guess game

--- Week 1/Personal/guessgame.py
+++ Week 1/Personal/guessgame.py
@@ -1,10 +1,9 @@
 import random
 i = random.randint(0,10000)
-for i in range(10):
+for i in range(3):
     a = int(input("Enter a number below 10000: "))
     if i == a: 
         print("Correct guess")
-        end()
     elif i < a: 
         print("Guess a lower number")
     else: 
